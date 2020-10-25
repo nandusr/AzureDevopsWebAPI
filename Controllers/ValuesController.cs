2f247abf4639f20efff4b90955f7ebe628451714
nandusr <68333379+nandusr@users.noreply.github.com>
2020-10-25 21:48:25 +0530

    Update ValuesController.cs

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace HelloWorld.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return "value-updated";
         }
 
         // POST api/values
